b71b917fd08166240bf13c353512482787c55991
NguyennQuoccKhanhh <quockhanhh129@gmail.com>
2025-03-08 00:03:35 +0700

    khanh update v.01 course

--- database/seeders/LessonSeeder.php
+++ database/seeders/LessonSeeder.php
@@ -9,10 +9,67 @@ class LessonSeeder extends Seeder
 {
     public function run()
     {
-        Lesson::create(['courseId' => 1, 'name' => 'Lesson 1', 'slug' => 'lesson-1', 'videoUrl' => 'http://example.com/video1', 'description' => 'Description for Lesson 1', 'duration' => 60, 'orderNumber' => 1, 'isPreview' => true, 'totalView' => 0, 'totalComment' => 0]);
-        Lesson::create(['courseId' => 1, 'name' => 'Lesson 2', 'slug' => 'lesson-2', 'videoUrl' => 'http://example.com/video2', 'description' => 'Description for Lesson 2', 'duration' => 90, 'orderNumber' => 2, 'isPreview' => false, 'totalView' => 0, 'totalComment' => 0]);
-        Lesson::create(['courseId' => 2, 'name' => 'Lesson 3', 'slug' => 'lesson-3', 'videoUrl' => 'http://example.com/video3', 'description' => 'Description for Lesson 3', 'duration' => 120, 'orderNumber' => 3, 'isPreview' => false, 'totalView' => 0, 'totalComment' => 0]);
-        Lesson::create(['courseId' => 2, 'name' => 'Lesson 4', 'slug' => 'lesson-4', 'videoUrl' => 'http://example.com/video4', 'description' => 'Description for Lesson 4', 'duration' => 150, 'orderNumber' => 4, 'isPreview' => false, 'totalView' => 0, 'totalComment' => 0]);
-        Lesson::create(['courseId' => 3, 'name' => 'Lesson 5', 'slug' => 'lesson-5', 'videoUrl' => 'http://example.com/video5', 'description' => 'Description for Lesson 5', 'duration' => 180, 'orderNumber' => 5, 'isPreview' => false, 'totalView' => 0, 'totalComment' => 0]);
+        // Video Lessons
+        Lesson::create([
+            'courseId' => 1,
+            'name' => 'Giới thiệu khóa học',
+            'slug' => 'gioi-thieu-khoa-hoc',
+            'type' => 'video',
+            'description' => 'Bài học giới thiệu tổng quan về khóa học',
+            'orderNumber' => 1,
+            'isPreview' => true,
+            'totalView' => 0,
+            'totalComment' => 0
+        ]);
+
+        Lesson::create([
+            'courseId' => 1,
+            'name' => 'Bài học cơ bản 1',
+            'slug' => 'bai-hoc-co-ban-1',
+            'type' => 'video',
+            'description' => 'Nội dung bài học cơ bản 1',
+            'orderNumber' => 2,
+            'isPreview' => false,
+            'totalView' => 0,
+            'totalComment' => 0
+        ]);
+
+        // Zoom Lessons
+        Lesson::create([
+            'courseId' => 1,
+            'name' => 'Buổi thảo luận trực tuyến 1',
+            'slug' => 'buoi-thao-luan-truc-tuyen-1',
+            'type' => 'zoom',
+            'description' => 'Buổi học trực tuyến qua Zoom',
+            'orderNumber' => 3,
+            'isPreview' => false,
+            'totalView' => 0,
+            'totalComment' => 0
+        ]);
+
+        // More Video Lessons
+        Lesson::create([
+            'courseId' => 2,
+            'name' => 'Bài học nâng cao 1',
+            'slug' => 'bai-hoc-nang-cao-1',
+            'type' => 'video',
+            'description' => 'Nội dung bài học nâng cao 1',
+            'orderNumber' => 1,
+            'isPreview' => false,
+            'totalView' => 0,
+            'totalComment' => 0
+        ]);
+
+        Lesson::create([
+            'courseId' => 2,
+            'name' => 'Bài học nâng cao 2',
+            'slug' => 'bai-hoc-nang-cao-2',
+            'type' => 'video',
+            'description' => 'Nội dung bài học nâng cao 2',
+            'orderNumber' => 2,
+            'isPreview' => false,
+            'totalView' => 0,
+            'totalComment' => 0
+        ]);
     }
 }
